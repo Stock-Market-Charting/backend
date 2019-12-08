6e64f7079f2bddfa8fe9c2f6ccc3d750c9f45c7e
lauro <lauro_lx@163.com>
2019-12-08 13:30:58 +0800

    add registry, gateway, stock-exchange services

--- stock-exchange-service/src/main/java/com/smc/se/StockExchangeServiceApplication.java
+++ stock-exchange-service/src/main/java/com/smc/se/StockExchangeServiceApplication.java
@@ -1,15 +1,15 @@
-package com.smc.user;
+package com.smc.se;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
-public class UserServiceApplication {
+public class StockExchangeServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(StockExchangeServiceApplication.class, args);
     }
 }
